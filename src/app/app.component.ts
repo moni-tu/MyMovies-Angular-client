efb2c3b12cb22cd22b457b0e356cac1a6030d717
moni-tu <turani.monica@gmail.com>
2022-05-11 22:21:57 +0200

    re-factor: pass the Angular Material dialog in the constructor
    Add openUserRegistrationDialog function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
+  title = 'myMovies-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  // This is the function that will open the dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+    // Assigning the dialog a width
+      width: '280px'
+    });
+  }
 }
