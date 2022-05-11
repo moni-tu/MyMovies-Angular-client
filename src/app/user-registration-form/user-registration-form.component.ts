7561a8d58e838b1ae44419791982857906201069
moni-tu <turani.monica@gmail.com>
2022-05-11 22:27:28 +0200

    console.log() to test registerUser function

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -32,6 +32,7 @@ export class UserRegistrationFormComponent implements OnInit {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
     // Logic for a successful user registration goes here! (To be implemented)
     this.dialogRef.close(); // This will close the modal on success!
+    console.log(result);
     this.snackBar.open(result, 'OK', {
         duration: 2000
     });
