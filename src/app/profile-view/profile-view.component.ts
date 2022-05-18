6d63683582e1f9a00e1e737ee08b346c461a5073
moni-tu <turani.monica@gmail.com>
2022-05-18 13:26:54 +0200

    re-factor: add relevant functions for profile view (delete favo movie, add fav movie, delete user profile etc)

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -10,6 +10,11 @@ import { FetchApiDataService } from '../fetch-api-data.service';
   styleUrls: ['./profile-view.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
+  user: any = {};
+  username: any = localStorage.getItem('user');
+  movies: any[] = [];
+  Favorites: any[] = [];
+  displayElement: boolean = false
 
   constructor(
     public fetchApiData: FetchApiDataService,
@@ -19,6 +24,67 @@ export class ProfileViewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  // get user profile info
+  getProfile(): void {
+    const username = localStorage.getItem('user');
+    if (username) {
+      // getUserProfile() is in fetchapidataservice.ts
+      this.fetchApiData.getUserProfile().subscribe((res: any) => {
+        this.user = res;
+        console.log(this.user);
+        return this.user;
+      });
+    }
+  }
+
+  getFavoriteMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      this.movies.forEach((movie: any) => {
+        if (this.user.Favorites.includes(movie._id)) {
+          this.Favorites.push(movie);
+        }
+      });
+    });
+    console.log(this.Favorites);
+  }
+
+  // delete favorite movie
+  deleteFavoriteMovies(_id: string, title: string): void {
+    // deleteFavoriteMovie is in fetchapi dataservice
+    this.fetchApiData.deleteFavoriteMovie(_id).subscribe((res: any) => {
+      this.snackBar.open(`Successfully removed ${title} from favorite movies.`, 'OK', {
+        duration: 4000, verticalPosition: 'top'
+      });
+      setTimeout(function () {
+        window.location.reload();
+      }, 4000);
+    });
+  }
+
+  //edit user info
+  /* openEditUserProfile(): void {
+    this.dialog.open(UserEditComponent, {
+      width: '500px',
+      panelClass: 'edit-user-custom',
+    });
+  } */
+
+  deleteUserProfile(): void {
+    if (confirm('Are your sure you want to delete your account? This can\'t be undone.')) {
+      this.router.navigate(['welcome']).then(() => {
+        this.snackBar.open('Your account has been deleted', 'OK', {
+          duration: 6000,
+          verticalPosition: 'top'
+        });
+      });
+      this.router.navigate(['welcome'])
+      this.fetchApiData.deleteUserProfile().subscribe(() => {
+        localStorage.clear();
+      });
+    }
   }
 
 }
