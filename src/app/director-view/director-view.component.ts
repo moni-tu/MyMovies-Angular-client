0b2fafee872204e471cd7003fafc497a9bdcf3dc
moni-tu <turani.monica@gmail.com>
2022-05-18 11:56:43 +0200

    feat: import and use Inject and MAT_DIALOG_DATA

--- src/app/director-view/director-view.component.ts
+++ src/app/director-view/director-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-director-view',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    // Injects data from MovieCardComponent into DirectorCardComponent using the MAT_DIALOG_DATA injection token.
+    // The data becomes a property on the class and is available to be output in the template.
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      name: string,
+      bio: string,
+      birth: string,
+    }
+  ) { }
 
   ngOnInit(): void {
   }
